cc4f04cca3d3fc0485525976916008010b2e5895
Josiel <josielalv@hotmail.com>
2020-01-16 12:53:47 -0300

    Componente e Propriedade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-// Componente
-// Estado
-// Propriedade
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: 
+
+import Header from './Header';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Header title="Titulo 1" />
+      <Header title="Titulo 2" />
+      <Header title="Titulo 4" />
+    </>
   );
 }
 
