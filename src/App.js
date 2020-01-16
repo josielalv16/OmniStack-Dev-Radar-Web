5bbb44c37f731767df1b0c0900b20e05d372af52
Josiel <josielalv@hotmail.com>
2020-01-16 13:03:48 -0300

    Criação de styles e alterações iniciais

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import './global.css';
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
